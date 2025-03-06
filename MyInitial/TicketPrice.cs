6a90623b4ccf4f9608dc018fd38c8373449bf94b
Nick Bentley <nicholas.bentley@student.tafesa.edu.au>
2025-03-07 08:18:36 +1030

    commit the child discount changes

--- MyInitial/TicketPrice.cs
+++ MyInitial/TicketPrice.cs
@@ -10,13 +10,15 @@ namespace Ticketing
         private int section;
         private int quantity;
         private bool discount;
+        private bool isChild;
         private decimal amountDue;
         private decimal mPrice;
 
         const decimal mdecBalcony = 35.5m;
         const decimal mdecGeneral = 28.75m;
         const decimal mdecBox = 62.0m;
-        const decimal mdecDiscount = 5.0m; 
+        const decimal mdecDiscount = 5.0m;
+        const decimal mdecDiscountChild = 10.0m;
 
         private int Section
         {
@@ -35,20 +37,25 @@ namespace Ticketing
             get { return discount; }
             set { discount = value; }
         }
-
-         public decimal AmountDue
+          private bool IsChild // Added property
+        {
+            get { return isChild; }
+            set { isChild = value; }
+        }
+        public decimal AmountDue
         {
             get { return amountDue; }
             set { amountDue = value; }
         }
 
     // Constructor for TcicketPrice
-    public TicketPrice(int section, int quantity, bool discount)
+    public TicketPrice(int section, int quantity, bool discount, bool isChild)
     {
         Section = section;
         Quantity = quantity;
         Discount = discount;
-        AmountDue = amountDue;
+         IsChild = isChild;
+         AmountDue = amountDue;
     }
 
      public void calculatePrice()
@@ -69,7 +76,14 @@ namespace Ticketing
          if (discount)
          { mPrice -= mdecDiscount; }
 
-         AmountDue = mPrice * quantity;
+           
+
+            if (isChild)
+            {
+                mPrice -= mdecDiscountChild;
+            }
+
+            AmountDue = mPrice * quantity;
 
      }
     }
